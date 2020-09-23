87fa07059257e5a2fe45d3922985eddb63fd7ca5
samiandreas70@gmail.com <samiandreas70@gmail.com>
2020-09-23 17:25:10 -0400

    added, prompts and confirms,and math.random functions

--- script.js
+++ script.js
@@ -5,13 +5,21 @@ var generateBtn = document.querySelector("#generate");
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  var prompt = ("how many characters");
 
   passwordText.value = password;
 
 }
-funtion generatePassword(){
-  return ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+function generatePassword() { 
+  prompt ("How many characters would you like your password to contain");
+  confirm ("click ok to confirm including special characters.");
+  confirm ("click ok to include numeric characters.");
+  confirm ("click ok to confirm including lowercase Characters.");
+  confirm ("click ok to confirm uppercase characters.");
+
+  return Math.floor(Math.random() * Math.floor(128));
+  
 }
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
- 
